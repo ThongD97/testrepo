85845332546f4dab3956db46f410d409cb28c09f
ThongD97 <106470038+ThongD97@users.noreply.github.com>
2022-06-02 06:48:47 +0700

    Add files via upload

---
+++ 01.input.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+print("Hello" + name)
+
+num_1 = input("Enter num 1: ")
+num_2 = input("Enter num 2: ")
+result = float(num_1) + float(num_2)
+
+print(result)
+
+print("-------")
+
+color = input("Enter a color: ")
+prural_noun = input("Enter a Prural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Rose are " + color)
+print(prural_noun + " are blue")
+print("I love " + celebrity)
